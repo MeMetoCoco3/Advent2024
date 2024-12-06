8fa33f781256f9d266a96469857b896c5b829b75
MeMetoCoco3 <torresmartinezvidal@gmail.com>
2024-12-06 08:52:50 +0000

    4 day completedgit add *

--- 04.py
+++ 04.py
@@ -1,26 +1,28 @@
-with open("044.txt") as f:
-    test = [line.strip() for line in f.readlines()[:-1] if line.strip()]
-
-directions = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
-directions2 = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
-
-count = 0
+with open("04.txt") as f:
+    test = [line.strip() for line in f.readlines() if line.strip()]
+print(test[0])
+print(test[-1])
 
 # First Answer
+directions = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+count = 0
 for row in range(len(test)):
     for col in range(len(test[0])):
         for dx, dy in directions:
             if 0 <= row + dx * 3 < len(test) and 0 <= col + dy * 3 < len(test[0]):
-                if (
-                    test[row][col] == "X"
-                    and test[row + dx][col + dy] == "M"
-                    and test[row + dx * 2][col + dy * 2] == "A"
-                    and test[row + dx * 3][col + dy * 3] == "S"
-                ):
-                    count += 1
+                try:
+                    if (
+                        test[row][col] == "X"
+                        and test[row + dx][col + dy] == "M"
+                        and test[row + dx * 2][col + dy * 2] == "A"
+                        and test[row + dx * 3][col + dy * 3] == "S"
+                    ):
+                        count += 1
+                except IndexError:
+                    pass
 print(count)
-count = 0
 # Second Answer
+count = 0
 edges = ["MAS", "SAM"]
 for row in range(len(test)):
     for col in range(len(test[0])):
@@ -28,17 +30,16 @@ for row in range(len(test)):
             continue
 
         if (
-            0 <= row - 1
-            and row + 1 < len(test)
-            and 0 <= col - 1
-            and col + 1 < len(test[0])
+            0 <= row - 1 < len(test)
+            and 0 <= row + 1 < len(test)
+            and 0 <= col - 1 < len(test[0])
+            and 0 <= col + 1 < len(test[0])
         ):
-            diagonal1 = test[row - 1][col - 1] + test[row][col] + test[row + 1][col + 1]
-            diagonal2 = test[row - 1][col + 1] + test[row][col] + test[row + 1][col - 1]
-            if (diagonal1 == "MAS" or diagonal1 == "SAM") and (
-                diagonal2 == "MAS" or diagonal2 == "SAM"
-            ):
+            bl = test[row - 1][col - 1]
+            tr = test[row + 1][col + 1]
+            br = test[row - 1][col + 1]
+            tl = test[row + 1][col - 1]
+            sortedLetters = sorted([bl, tr, br, tl])
+            if sortedLetters == ["M", "M", "S", "S"] and bl != tr:
                 count += 1
-
-
 print(count)
